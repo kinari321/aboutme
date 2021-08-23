6ff2d7cda5b2680f1123b964b81e484a9da496c0
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-08-23 11:45:37 +0900

    edit nav-bar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <v-app>
-    <v-navigation-drawer app v-model="drawer" clipped>Navigation Lists</v-navigation-drawer>
-    <v-app-bar color="primary" dark app clipped-left>
-      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
-      <v-tool-title>Nav Bar</v-tool-title>
+    <v-app-bar
+      color="#6A76AB"
+      dark
+      class="text-sm-h5"
+    >
+      <v-tool-title>Kinari Shirakawa</v-tool-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
       <v-btn text to="/"><v-icon>mdi-account-outline</v-icon>About me</v-btn>
         <v-btn text to="/contact"><v-icon>mdi-email-outline</v-icon>Contact</v-btn>
       </v-toolbar-items>
     </v-app-bar>
-    <v-main>
+    <v-main class="grey lighten-3">
       <router-view />
     </v-main>
-    <v-footer color="primary" dark app>
+    <v-footer color="#6A76AB" dark app>
       Footer
     </v-footer>
   </v-app>
@@ -34,4 +36,8 @@ export default {
 .v-btn{
   text-transform: none
 }
+.v-main{
+  height: 1400px;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,49 @@
 <template>
-  <hello-world />
+  <div>
+     <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="1"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+              class="grey lighten-3"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="8"
+          >
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="3"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
